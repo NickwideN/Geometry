b3a9815a86de421f149d453c2eef14a40c6589b5
NickwideN <37487668+NickwideN@users.noreply.github.com>
2018-04-02 12:36:04 +0300

    Переменное колво аргументов

--- Geometry/Geometry/Vector.cpp
+++ Geometry/Geometry/Vector.cpp
@@ -1,15 +1,16 @@
 #include"Vector.h"
-
+#include<stdarg.h>
 Vector::Vector() {
     for (int i = 0; i < DIMENTION; ++i) {
         coordinates[i] = default_value;
     }
 }
 
-Vector::Vector(coordinate_t coors, ...) { //doesn't have a cheking if it inputs number of coors that less than DIMENTION
-    coordinate_t *pointer_coor = &coors;
-    for (int i = 0; i < DIMENTION; ++i) {
-        coordinates[i] = *pointer_coor;
-        ++pointer_coor;
-    }
+Vector::Vector(coordinate_t coor_1, ...) { //doesn't have a cheking if it inputs number of coors that less than DIMENTION
+        va_list pointer_coor;             //--объ€вление указател€
+        va_start(pointer_coor, coor_1);            //--инициализаци€ указател€
+        coordinates[0] = coor_1;
+        for (int i = 1; i < DIMENTION; ++i)
+            coordinates[i] += va_arg(pointer_coor, coordinate_t);        //--перемещение указател€ 
+        va_end(pointer_coor);                //--Ђзакрытиеї указател€
 }
