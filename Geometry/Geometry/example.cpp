279f517be19b83c170c9b3db9209f402ae11aba0
NickwideN <37487668+NickwideN@users.noreply.github.com>
2019-11-29 14:45:59 +0300

    Create example.cpp

---
+++ Geometry/Geometry/example.cpp
@@ -0,0 +1,226 @@
+#include"Geometry.h"
+#include<iostream>
+#include<iomanip>
+using namespace Geometry;
+using namespace std;
+// B
+/*
+int main() {
+Vector radius_vector[4];
+for (int i = 0; i < 4; ++i) {
+cin >> radius_vector[i];
+}
+Vector vector_1 = radius_vector[1] - radius_vector[0];
+Vector vector_2 = radius_vector[3] - radius_vector[2];
+cout.fill('0');
+cout.width();
+cout << std::setprecision(9) << fixed << abs(vector_1) << ' ' << abs(vector_2) << '\n';
+cout << vector_1 + vector_2 << '\n';
+cout << scalar_product(vector_1, vector_2) << ' ' << skew_product(vector_1, vector_2) << '\n';
+cout << abs(skew_product(vector_1, vector_2)) / 2 << '\n';
+getchar();
+getchar();
+return 0;
+}
+*/
+
+// C right
+/*
+int main() {
+int coef[6];
+for (int i = 0; i < 6; ++i) {
+cin >> coef[i];
+}
+Line line_1(coef[0], coef[1], coef[2]);
+Line line_2(coef[3], coef[4], coef[5]);
+cout << std::setprecision(9) << fixed << line_1.get_direction() << '\n' << line_2.get_direction() << '\n';
+if (are_intersecting(line_1, line_2)) {
+cout << intersection(line_1, line_2);
+}
+else if (are_parallel(line_1, line_2)) {
+cout << distance_between(line_1, line_2);
+}
+getchar();
+getchar();
+return 0;
+}
+*/
+// D  right
+/*
+int main() {
+    Point C;
+    Point A;
+    Point B;
+    cin >> C >> A >> B;
+    Line l(A, B);
+    Segment s(A, B);
+    Ray r(A, B);
+    cout << (l.has_point(C) ? "YES" : "NO") << '\n';
+    cout << (r.has_point(C) ? "YES" : "NO") << '\n';
+    cout << (s.has_point(C) ? "YES" : "NO") << '\n';
+    getchar();
+    getchar();
+    return 0;
+}
+*/
+
+// E // right
+/*
+int main() {
+Point C;
+Point A;
+Point B;
+cin >> C >> A >> B;
+cout << setprecision(9) << fixed;
+cout << C.distance_to(Line(A, B)) << '\n';
+cout << C.distance_to(Ray(A, B)) << '\n';
+cout << C.distance_to(Segment(A, B)) << '\n';
+getchar();
+getchar();
+return 0;
+}
+*/
+
+// F //right
+/*
+int main() {
+    Segment a;
+    Segment b;
+    cin >> a;
+    cin >> b;
+    cout << (a.has_intarsection_with(b) ? "YES" : "NO");
+    getchar();
+    getchar();
+    return 0;
+}
+*/
+
+// G // right
+/*
+int main() {
+    Segment a;
+    Segment b;
+    cin >> a;
+    cin >> b;
+    cout << setprecision(9) << fixed << distance_between(a, b);
+    getchar();
+    getchar();
+    return 0;
+}
+*/
+// H   wrong
+/*
+int main() {
+    int points_cnt;
+    cin >> points_cnt;
+    Polygon pol(points_cnt);
+    cin >> pol;
+    cout << (pol.is_convex() ? "YES" : "NO");
+    getchar();
+    getchar();
+    return 0;
+}
+*/
+// I  //right
+/*
+int main() {
+    int points_cnt;
+    cin >> points_cnt;
+    Point check_point;
+    Polygon pol(points_cnt);
+    cin >> check_point;
+    cin >> pol;
+    cout << (pol.has_point(check_point) ? "YES" : "NO");
+    getchar();
+    getchar();
+    return 0;
+}
+*/
+
+// J  true
+/*
+int main() {
+    int points_cnt;
+    cin >> points_cnt;
+    Polygon pol(points_cnt);
+    cin >> pol;
+    cout << area(pol);
+    getchar();
+    getchar();
+    return 0;
+}
+*/
+
+// K
+int main() {
+    int points_cnt;
+    cin >> points_cnt;
+    Polygon pol(points_cnt);
+    cin >> pol;
+    Polygon convex_hull_pol = convex_hull(pol);
+    cout << convex_hull_pol.get_points_cnt() << '\n';
+    cout << convex_hull_pol << '\n';
+    cout << setprecision(1) << fixed << area(convex_hull_pol);
+    getchar();
+    getchar();
+    return 0;
+}
+/*
+8
+1 2 5 4
+1 2 5 8
+2 2 5 4
+1 4 5 2
+10
+0 0
+5 0
+5 2
+5 4
+3 4
+3 5
+2 2
+2 2
+0 9
+3 2
+10
+6 0
+6 2
+0 0
+8 4
+11 4
+9 7
+4 4
+5 7
+2 7
+4 2
+4
+1 2 
+3 2
+1 2
+5 2
+*/
+
+
+// Checking Polygon
+/*
+int main() {
+Point a(4, 1);
+Point b(2, 2);
+Point c(0, 6);
+Point d(0, 1);
+Point e(1, 1);
+Segment seg(a, c);
+Point p[5] = { a, b, a, d, e };
+Polygon pol1(5, p);
+Polygon pol2(4, a, b, c, d);
+pol1 = pol2;
+cout << area(pol2) << '\n';
+cout << pol2.has_point(e);
+cout << pol2.is_convex();
+cout << pol2.has_intarsection_with(seg);
+cout << pol2.has_point(a);
+cout << pol2.has_point(c);
+getchar();
+getchar();
+return 0;
+}*/
